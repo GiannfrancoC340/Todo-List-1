233a009c493acae80565ee41377d5800e0ce0327
Giannfranco Crovetto <gcrovetto2021@fau.edu>
2025-04-06 12:45:02 -0400

    Adding the rest of the files

---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-todo-list',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './todo-list.component.html',
+  styleUrl: './todo-list.component.css'
+})
+export class TodoListComponent {
+  newTask = '';
+  tasks: string[] = ['Learn Angular basics', 'Create first component', 'Build a simple app', 'Experiment with app'];
+
+  addTask() {
+    if (this.newTask.trim()) {
+      this.tasks.push(this.newTask);
+      this.newTask = '';
+    }
+  }
+
+  removeTask(index: number) {
+    this.tasks.splice(index, 1);
+  }
+}
